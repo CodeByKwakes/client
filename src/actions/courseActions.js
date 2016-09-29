581737d3eee77c0fddc502107cb98c8f6d08983f
KwakesProject <kwakes@me.com>
2016-09-29 17:28:24 +0100

    async chapter. have manged to get data from mock api

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,6 +1,21 @@
 /*This file hold our Course related Action Creators*/
 import * as types from './actionTypes';
+import courseApi from '../api/mockCourseApi';
 
-export function createCourse(course){
+/*export function createCourse(course){
   return { type: types.CREATE_COURSE, course};
+}*/
+export function loadCoursesSussess(courses){
+  return { type: types.LOAD_COURSES_SUCCESS, courses};
+}
+
+/*Thunk*/
+export function loadCourses(){
+  return function(dispatch){
+    return courseApi.getAllCourses().then(courses => {
+      dispatch(loadCoursesSussess(courses));
+    }).catch(error => {
+      throw(error);
+    });
+  };
 }
