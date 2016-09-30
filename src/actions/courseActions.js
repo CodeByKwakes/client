711d518f2f2fbe4b2b5912271d347fa001288eb7
KwakesProject <kwakes@me.com>
2016-09-30 15:44:03 +0100

    save and update courses added

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -9,11 +9,33 @@ export function loadCoursesSussess(courses){
   return { type: types.LOAD_COURSES_SUCCESS, courses};
 }
 
+export function createCourseSuccess(course) {
+  return {type: types.CREATE_COURSE_SUCCESS, course};
+}
+
+export function updateCourseSuccess(course) {
+  return {type: types.UPDATE_COURSE_SUCCESS, course};
+}
+
+
 /*Thunk*/
+// Functions below handle asynchronous calls.
+// Each returns a function that accepts a dispatch.
+// These are used by redux-thunk to support asynchronous interactions.
 export function loadCourses(){
   return function(dispatch){
     return CourseApi.getAllCourses().then(courses => {
       dispatch(loadCoursesSussess(courses));
+    }).catch(error => {
+      throw(error);
+    });
+  };
+}
+
+export function saveCourse(course) {
+  return function (dispatch, getState) {
+    return CourseApi.saveCourse(course).then(course => {
+      course.id ? dispatch(updateCourseSuccess(course)) : dispatch(createCourseSuccess(course));
     }).catch(error => {
       throw(error);
     });
