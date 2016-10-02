6334bdc11ebd82eb257188ded18401e88b104f46
KwakesProject <kwakes@me.com>
2016-10-02 18:36:40 +0100

    added button saving function

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -10,7 +10,8 @@ class ManageCoursePage extends React.Component {
 
     this.state = {
       course: Object.assign({}, props.course),
-      errors: {}
+      errors: {},
+      saving: false
     };
 
     /*bind*/
@@ -36,11 +37,13 @@ class ManageCoursePage extends React.Component {
 
   saveCourse(event) {
     event.preventDefault();
+    this.setState({saving: true });
     this.props.actions.saveCourse(this.state.course)
       .then(() => this.redirect());
   }
 
   redirect() {
+    this.setState({saving: false });
     this.context.router.push('/courses');
   }
 
@@ -53,6 +56,7 @@ class ManageCoursePage extends React.Component {
         onSave={this.saveCourse}
         course={this.state.course}
         errors={this.state.errors}
+        saving={this.state.saving}
         />
     );
   }
