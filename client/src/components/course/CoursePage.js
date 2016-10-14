c9ff1679bb897d6e6d543058129991f57e3ee685
KwakesProject <kwakes@me.com>
2016-10-14 17:35:07 +0100

    clean up

--- client/src/components/course/CoursePage.js
+++ client/src/components/course/CoursePage.js
@@ -1,46 +1,46 @@
 /*Container Component*/
-import React, {PropTypes} from 'react';
+import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as courseActions from '../../actions/courseActions';
 import CourseList from './CourseList';
-import {browserHistory} from 'react-router';
+import { browserHistory } from 'react-router';
 
 class CoursesPage extends React.Component {
 
   /*======Construtor====================*/
-  constructor(props, context){
+  constructor(props, context) {
     super(props, context);
     this.redirectToAddCoursePage = this.redirectToAddCoursePage.bind(this);
 
   }
 
-/*======End Construtor====================*/
+  /*======End Construtor====================*/
 
-/*=========Child Functions=============================*/
+  /*=========Child Functions=============================*/
 
-courseRow(course, index){
-  return <div key={index}>{course.title}</div>;
-}
+  courseRow(course, index) {
+    return <div key={index}>{course.title}</div>;
+  }
 
-redirectToAddCoursePage(){
+  redirectToAddCoursePage() {
     browserHistory.push('/course');
-}
+  }
 
-/*==============End Child Functions==============================*/
-    render() {
-      const { courses } = this.props;
-        return (
-            <div>
-                <h1>Courses</h1>
-                <input type="submit"
-                        value="Add Course"
-                        className="btn btn-primary"
-                        onClick={this.redirectToAddCoursePage}/>
-                <CourseList courses={courses} />
-            </div>
-        );
-    }
+  /*==============End Child Functions==============================*/
+  render() {
+    const { courses } = this.props;
+    return (
+      <div>
+        <h1>Courses</h1>
+        <input type="submit"
+          value="Add Course"
+          className="btn btn-primary"
+          onClick={this.redirectToAddCoursePage} />
+        <CourseList courses={courses} />
+      </div>
+    );
+  }
 }
 
 /*===============PropTypes====================*/
@@ -50,9 +50,9 @@ CoursesPage.propTypes = {
   courses: PropTypes.array.isRequired,
   actions: PropTypes.object.isRequired
 };
- /*======End PropTypes====================*/
+/*======End PropTypes====================*/
 
- /*==============Redux Connect and relatived functions===========================*/
+/*==============Redux Connect and relatived functions===========================*/
 function mapStateToProps(state, ownProps) {
   return {
     courses: state.courses
